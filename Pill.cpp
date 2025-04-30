66fe245e12bf8e72cfb690dce2affc4b78ecc533
laurdang <laurdang@bu.edu>
2025-04-30 11:53:38 -0400

    Add files via upload

--- Pill.cpp
+++ Pill.cpp
@@ -6,7 +6,7 @@ Pill::Pill() {
   filename = "DEFAULT";
 }
 
-Pill::Pill(unsigned long ID,String IDlabel, String filename) {
+Pill::Pill(unsigned long ID, String IDlabel, String filename) {
   this->ID = ID;
   this->IDlabel = IDlabel;
   this->filename = filename;
@@ -15,14 +15,21 @@ Pill::Pill(unsigned long ID,String IDlabel, String filename) {
 void Pill::printInfo() {
   Serial.println();
   Serial.println("Pill Info:");
-  Serial.print("ID (label): "); Serial.println(IDlabel);
-  Serial.print("Name: "); Serial.println(filename);
+  Serial.print("ID (label): ");
+  Serial.println(IDlabel);
+  Serial.print("Name: ");
+  Serial.println(filename);
 }
 
 Pill pills[] = {
-  Pill(0xCADC9A3F, "CADC9A3F", "CAYENNE_PEPPER.WAV"),
-  Pill(0xDADC9A3F, "DADC9A3F", "OXYCODON.WAV"),
-  Pill(0xAADC9A3F, "AADC9A3F", "ATORVASTATIN.WAV"),
-  Pill(0x6ADC9A3F, "6ADC9A3F", "XANAX.WAV"),
-  Pill(0x7ADC9A3F, "7ADC9A3F", "VIAGRA.WAV"),
+  Pill(3135019583, "ato", "3.WAV"),
+  Pill(1792842303, "xan", "4.WAV"),
+  Pill(2061277759, "via", "5.WAV"),
+
+  // Other example objects:
+  // Pill(3403455039, "cay", "1.WAV"),
+  // Pill(3671890495, "oxy", "2.WAV"),
+  // Pill(2598148671, "tum", "6.WAV"),
+  // Pill(2866584127, "ita", "7.WAV"),
+  // Pill(2329713215, "tye", "8.WAV"),
 };
